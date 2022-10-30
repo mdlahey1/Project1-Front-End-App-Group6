363a0dead892f1124b683fb7cba31f4ca4e2a3d6
Colin McMurtray <112663656+codingColinMcM@users.noreply.github.com>
2022-10-30 18:54:41 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,26 @@
-var requestUrl1 = 'https://api.themoviedb.org/3/search/movie?api_key=a6091da1f77938caf706363106cf0289&query=Jaws&include_image_language';
+var posterContainer = document.querySelector('#poster')
+var requestUrl = 'https://api.themoviedb.org/3/search/movie?api_key=a6091da1f77938caf706363106cf0289&query=Jaws&include_image_language';
 
 function getApi(url) {
     fetch(url)
     .then(response => response.json())
-    .then(response => console.log(response.results[0].poster_path))
+    .then(response => displayPoster(response.results[0].poster_path))
+    
     .catch(err => console.error(err));
 };
 
-getApi(requestUrl1)
+function displayPoster(imagePath) {
+
+    var card = document.createElement('div');
+    var imageUrl = `https://image.tmdb.org/t/p/original${imagePath}`;
+    var posterIcon = document.createElement('img');
+
+    card.append(posterIcon);
+
+    card.setAttribute('class', 'col-12');
+    posterIcon.setAttribute('src', imageUrl);
+
+    posterContainer.append(card);
+}
+
+getApi(requestUrl)
