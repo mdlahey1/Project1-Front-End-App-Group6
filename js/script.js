85670ad0d012cc502b93900d6adc89aef876fa79
Colin McMurtray <112663656+codingColinMcM@users.noreply.github.com>
2022-10-31 17:47:13 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
-var posterContainer = document.querySelector('#poster')
-var requestUrl = 'https://api.themoviedb.org/3/search/movie?api_key=a6091da1f77938caf706363106cf0289&query=Jaws&include_image_language';
+var posterContainer = document.querySelector('#posters')
+var requestUrl = 'https://api.themoviedb.org/3/search/movie?api_key=a6091da1f77938caf706363106cf0289&query=Avengers&include_image_language';
 var ottKey = '62e4d11a06msh5de2a416a1456c1p1475fajsn6107c95aedbc';
 
-// var fetchProv = "https://api.themoviedb.org/3/movie/578/watch/providers?api_key=a6091da1f77938caf706363106cf0289"
-
+var genreObj = {};
 
 function getApi(url) {
     fetch(url)
@@ -12,11 +11,18 @@ function getApi(url) {
     .catch(err => console.error(err));
 };
 
-function displayPoster(theseResults) {
+function getStreaming(url, i) {
+    fetch(url)
+    .then(response => response.json())
+    .then(response => displayIcon(response.results, i))
+    .catch(err => console.error(err));
+};
 
-    for (let i = 0; i < theseResults.length; i++){
+function displayPoster(posterResults) {
 
-        var imagePath = theseResults[i].poster_path
+    for (let i = 0; i < posterResults.length; i++){
+
+        var imagePath = posterResults[i].poster_path
 
         var card = document.createElement('div');
         var imageUrl = `https://image.tmdb.org/t/p/original${imagePath}`;
@@ -24,13 +30,57 @@ function displayPoster(theseResults) {
 
         card.append(posterIcon);
 
-        card.setAttribute('class', 'col-12');
+        card.setAttribute('class', 'col');
+        card.setAttribute('id', `col-${i}`)
         posterIcon.setAttribute('src', imageUrl);
+        posterIcon.setAttribute('id', 'poster');
 
+        
+
+        // Also get the streaming icons
+
+        var movieID = posterResults[i].id;
+        var fetchProv = `https://api.themoviedb.org/3/movie/${movieID}/watch/providers?api_key=a6091da1f77938caf706363106cf0289`;
+        console.log(i);
+        getStreaming(fetchProv, i);
+        
         posterContainer.append(card);
+
     }
 
-    
 }
 
-getApi(requestUrl)
+function displayIcon(flatRateResults, j) {
+
+    var card = document.getElementById(`col-${j}`);
+
+    try {
+
+        var flatRates = flatRateResults.US.flatrate;
+
+        for (let i = 0; i < flatRates.length; i++){
+
+            var imagePath = flatRates[i].logo_path;
+
+            
+            var imageUrl = `https://image.tmdb.org/t/p/original${imagePath}`;
+            var streamingIcon = document.createElement('img');
+
+            card.append(streamingIcon);
+
+            
+            streamingIcon.setAttribute('src', imageUrl);
+            streamingIcon.setAttribute('id', 'stream-icon');
+
+            
+        }
+    } catch (err) {
+
+        var noneAvailable = document.createElement('h6');
+        noneAvailable.innerHTML = 'No streaming services available for this movie';
+        card.append(noneAvailable)
+    }
+
+}
+
+getApi(requestUrl);
